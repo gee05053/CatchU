9fc02e89fcd2ab931bdddb6bbfa7dc98505e134d
gee05053 <gee05053@gmail.com>
2023-10-11 21:17:17 +0900

    modify loginpage layout

--- catchu-webui/src/Pages/LoginPage.tsx
+++ catchu-webui/src/Pages/LoginPage.tsx
@@ -1,5 +1,14 @@
 import React, { useContext, useState } from "react";
-import { Input, Button, Card, Divider, Form, message } from "antd";
+import {
+	Input,
+	Button,
+	Card,
+	Divider,
+	Form,
+	message,
+	Row,
+	Col,
+} from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import axios from "axios";
 import { LoginContext } from "../App";
@@ -36,94 +45,93 @@ const LoginPage: React.FC = () => {
 		}
 	};
 	return (
-		<>
-			{contextHolder}
-			<Form
-				onFinish={onFinish}
-				style={{
-					display: "flex",
-					justifyContent: "center",
-					alignItems: "center",
-					height: "80vh",
-					flexDirection: "column",
-				}}
+		<Row
+			justify="center"
+			align="middle"
+			style={{ height: "100vh" }}
+		>
+			<Col
+				xs={24}
+				sm={24}
+				md={12}
+				lg={6}
 			>
-				<Card
-					title="CatchU"
-					style={{ width: "30%", margin: "20px" }}
-				>
-					<Form.Item
-						name="id"
-						style={{ marginBottom: 0 }}
-					>
-						<Input
-							size="large"
-							placeholder="아이디"
-							prefix={<UserOutlined />}
-						/>
-					</Form.Item>
-					<Form.Item
-						name="password"
-						rules={[
-							{
-								required: true,
-								pattern: /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[_\W]).{8,}$/,
-								message:
-									"비밀번호는 특수문자를 포함한 8자리 이상이어야 합니다.",
-							},
-						]}
-					>
-						<Input
-							size="large"
-							type="password"
-							placeholder="비밀번호"
-							prefix={<LockOutlined />}
-						/>
-					</Form.Item>
-					<Form.Item>
-						<Button
-							block
-							htmlType="submit"
+				{contextHolder}
+				<Form onFinish={onFinish}>
+					<Card title="CatchU">
+						<Form.Item
+							name="id"
+							style={{ marginBottom: 0 }}
 						>
-							로그인
+							<Input
+								size="large"
+								placeholder="아이디"
+								prefix={<UserOutlined />}
+							/>
+						</Form.Item>
+						<Form.Item
+							name="password"
+							rules={[
+								{
+									required: true,
+									pattern: /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[_\W]).{8,}$/,
+									message:
+										"비밀번호는 특수문자를 포함한 8자리 이상이어야 합니다.",
+								},
+							]}
+						>
+							<Input
+								size="large"
+								type="password"
+								placeholder="비밀번호"
+								prefix={<LockOutlined />}
+							/>
+						</Form.Item>
+						<Form.Item>
+							<Button
+								block
+								htmlType="submit"
+							>
+								로그인
+							</Button>
+						</Form.Item>
+					</Card>
+					<Col style={{ textAlign: "center", marginTop: "5px" }}>
+						<Button
+							type="link"
+							style={{ padding: 0, color: "#8d8d8d" }}
+						>
+							아이디 찾기
 						</Button>
-					</Form.Item>
-				</Card>
-				<div>
-					<Button
-						type="link"
-						style={{ padding: 0, color: "#8d8d8d" }}
-					>
-						아이디 찾기
-					</Button>
-					<Divider
-						type="vertical"
-						style={{ backgroundColor: "#dadada" }}
-					/>
-					<Button
-						type="link"
-						style={{ padding: 0, color: "#8d8d8d" }}
-					>
-						비밀번호 찾기
-					</Button>
-					<Divider
-						type="vertical"
-						style={{ backgroundColor: "#dadada" }}
-					/>
-					<Button
-						type="link"
-						style={{ padding: 0, color: "#8d8d8d" }}
-						onClick={() => setSignUpOpen(true)}
-					>
-						회원가입
-					</Button>
-				</div>
-			</Form>
-			<SignUpModal
-				isSignUpOpen={isSiginUpOpen}
-				setSignUpOpen={setSignUpOpen}
-			/>
-		</>
+						<Divider
+							type="vertical"
+							style={{ backgroundColor: "#dadada" }}
+						/>
+						<Button
+							type="link"
+							style={{ padding: 0, color: "#8d8d8d" }}
+						>
+							비밀번호 찾기
+						</Button>
+						<Divider
+							type="vertical"
+							style={{ backgroundColor: "#dadada" }}
+						/>
+						<Button
+							type="link"
+							style={{ padding: 0, color: "#8d8d8d" }}
+							onClick={() => setSignUpOpen(true)}
+						>
+							회원가입
+						</Button>
+					</Col>
+				</Form>
+				<SignUpModal
+					isSignUpOpen={isSiginUpOpen}
+					setSignUpOpen={setSignUpOpen}
+				/>
+			</Col>
+		</Row>
 	);
 };
 
