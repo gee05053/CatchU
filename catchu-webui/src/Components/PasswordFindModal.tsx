2674f20b14e39ddc3bc9ba844d15ad8521716771
gee05053 <gee05053@gmail.com>
2023-10-13 21:17:51 +0900

    modify error handling

--- catchu-webui/src/Components/PasswordFindModal.tsx
+++ catchu-webui/src/Components/PasswordFindModal.tsx
@@ -18,39 +18,25 @@ const PasswordFindModal: React.FC<PasswordFindModalProps> = ({
 	const onClickFirstStep = async () => {
 		setIsLoading(true);
 		const inputValue = form.getFieldsValue();
-		if (
-			inputValue.id === undefined ||
-			inputValue.id === "" ||
-			inputValue.email === undefined ||
-			inputValue.email === ""
-		) {
-			messageApi.open({
-				type: "error",
-				content: "가입한 아이디와 이메일을 입력하세요.",
-			});
-		} else {
-			try {
-				const result = await axios.post(
-					"/account/findPassword",
-					inputValue,
-				);
-				if (result.data.checkNumber === undefined) {
-					messageApi.open({
-						type: "error",
-						content: "가입되어 있는 정보가 없습니다.",
-					});
-				} else {
-					setIsLoading(false);
-					setCheckNumber(result.data.checkNumber);
-					setCurrentStep(currentStep + 1);
-				}
-			} catch {
+		try {
+			const result = await axios.post("/account/findPassword", inputValue);
+			if (result.data.checkNumber === undefined) {
 				messageApi.open({
 					type: "error",
-					content: "알수 없는 오류가 발생했습니다.",
+					content: "가입되어 있는 정보가 없습니다.",
 				});
 				setIsLoading(false);
+			} else {
+				setIsLoading(false);
+				setCheckNumber(result.data.checkNumber);
+				setCurrentStep(currentStep + 1);
 			}
+		} catch {
+			messageApi.open({
+				type: "error",
+				content: "알수 없는 오류가 발생했습니다.",
+			});
+			setIsLoading(false);
 		}
 	};
 	const onClickSecondStep = () => {
@@ -66,36 +52,24 @@ const PasswordFindModal: React.FC<PasswordFindModalProps> = ({
 	};
 	const onClickThirdStep = async () => {
 		const inputValue = form.getFieldsValue();
-		if (
-			inputValue.password === undefined ||
-			inputValue.password === "" ||
-			inputValue.confirmPassword === undefined ||
-			inputValue === ""
-		) {
-			messageApi.open({
-				type: "error",
-				content: "비밀번호를 입력하세요",
-			});
-		} else {
-			try {
-				const result = await axios.post(
-					"/account/changePassword",
-					inputValue,
-				);
-				if (result.data.success) {
-					setCurrentStep(currentStep + 1);
-				} else {
-					messageApi.open({
-						type: "error",
-						content: "비밀번호 변경에 실패했습니다.",
-					});
-				}
-			} catch {
+		try {
+			const result = await axios.post(
+				"/account/changePassword",
+				inputValue,
+			);
+			if (result.data.success) {
+				setCurrentStep(currentStep + 1);
+			} else {
 				messageApi.open({
 					type: "error",
-					content: "비밀번호 변경 중 알 수 없는 오류로 실패했습니다.",
+					content: "비밀번호 변경에 실패했습니다.",
 				});
 			}
+		} catch {
+			messageApi.open({
+				type: "error",
+				content: "비밀번호 변경 중 알 수 없는 오류로 실패했습니다.",
+			});
 		}
 	};
 	const onClickClose = () => {
@@ -116,7 +90,12 @@ const PasswordFindModal: React.FC<PasswordFindModalProps> = ({
 					<Form.Item
 						name="id"
 						label="아이디"
-						required={true}
+						rules={[
+							{
+								required: true,
+								message: "이메일을 입력하세요.",
+							},
+						]}
 						style={{ marginTop: "10px" }}
 					>
 						<Input />
@@ -141,7 +120,13 @@ const PasswordFindModal: React.FC<PasswordFindModalProps> = ({
 			content: (
 				<Form.Item
 					name="checkNumber"
-					label="인증번호"
+					label="메일로 전송된 인증번호를 입력하세요."
+					rules={[
+						{
+							required: true,
+							message: "올바른 인증번호를 입력하세요",
+						},
+					]}
 				>
 					<Input />
 				</Form.Item>
@@ -153,20 +138,48 @@ const PasswordFindModal: React.FC<PasswordFindModalProps> = ({
 					<Form.Item
 						name="password"
 						label="변경할 비밀번호"
+						rules={[
+							{
+								required: true,
+								pattern: /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[_\W]).{8,}$/,
+								message:
+									"비밀번호는 특수문자를 포함한 8자리 이상이어야 합니다.",
+							},
+						]}
 					>
-						<Input />
+						<Input type="password" />
 					</Form.Item>
 					<Form.Item
 						name="confirmPassword"
 						label="비밀번호 확인"
+						rules={[
+							{
+								required: true,
+								message: "비밀번호를 입력하세요",
+							},
+							({ getFieldValue }) => ({
+								validator(_, value) {
+									if (!value || getFieldValue("password") === value) {
+										return Promise.resolve();
+									}
+									return Promise.reject(
+										new Error("비밀번호가 일치하지 않습니다."),
+									);
+								},
+							}),
+						]}
 					>
-						<Input />
+						<Input type="password" />
 					</Form.Item>
 				</>
 			),
 		},
 		{
-			content: <div>비밀번호 변경이 완료되었습니다.</div>,
+			content: (
+				<div>
+					비밀번호 변경이 완료되었습니다. 새로운 비밀번호로 로그인하세요.
+				</div>
+			),
 		},
 	];
 	return (
@@ -178,16 +191,9 @@ const PasswordFindModal: React.FC<PasswordFindModalProps> = ({
 			footer={[
 				currentStep < 3 ? (
 					<Button
+						htmlType="submit"
 						loading={isLoading}
-						onClick={() => {
-							if (currentStep === 0) {
-								onClickFirstStep();
-							} else if (currentStep === 1) {
-								onClickSecondStep();
-							} else {
-								onClickThirdStep();
-							}
-						}}
+						onClick={form.submit}
 					>
 						다음
 					</Button>
@@ -201,6 +207,15 @@ const PasswordFindModal: React.FC<PasswordFindModalProps> = ({
 			<Form
 				form={form}
 				layout="vertical"
+				onFinish={() => {
+					if (currentStep === 0) {
+						onClickFirstStep();
+					} else if (currentStep === 1) {
+						onClickSecondStep();
+					} else {
+						onClickThirdStep();
+					}
+				}}
 			>
 				{steps[currentStep].content}
 			</Form>
